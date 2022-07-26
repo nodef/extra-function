d3f67195b4c415c55067da40ed39bf36a3047e63
Subhajit Sahu <wolfram77@gmail.com>
2022-07-26 14:57:12 +0530

    :mango: simplify tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,5 +1,30 @@
-import {sleep}       from "extra-sleep";
-import * as funcxion from "../src";
+import {sleep}         from "extra-sleep";
+import * as funcxion   from "../src";
+import {is}            from "../src";
+import {isAsync}       from "../src";
+import {isGenerator}   from "../src";
+import {signature}     from "../src";
+import {name}          from "../src";
+import {parameters}    from "../src";
+import {arity}         from "../src";
+import {bind}          from "../src";
+import {negate}        from "../src";
+import {memoize}       from "../src";
+import {reverse}       from "../src";
+import {spread}        from "../src";
+import {unspread}      from "../src";
+import {wrap}          from "../src";
+import {unwrap}        from "../src";
+import {compose}       from "../src";
+import {composeRight}  from "../src";
+import {curry}         from "../src";
+import {curryRight}    from "../src";
+import {delay}         from "../src";
+import {limitUse}      from "../src";
+import {debounce}      from "../src";
+import {debounceEarly} from "../src";
+import {throttle}      from "../src";
+import {throttleEarly} from "../src";
 
 
 
@@ -27,21 +52,21 @@ test("example1", () => {
 
 
 test("is", () => {
-  var a = funcxion.is(Object.keys);
+  var a = is(Object.keys);
   expect(a).toBe(true);
-  var a = funcxion.is(() => 0);
+  var a = is(() => 0);
   expect(a).toBe(true);
-  var a = funcxion.is(async () => 0);
+  var a = is(async () => 0);
   expect(a).toBe(true);
-  var a = funcxion.is(0);
+  var a = is(0);
   expect(a).toBe(false);
 });
 
 
 test("isAsync", () => {
-  var a = funcxion.isAsync(async () => 0)
+  var a = isAsync(async () => 0)
   expect(a).toBe(true);
-  var a = funcxion.isAsync(() => 0)
+  var a = isAsync(() => 0)
   expect(a).toBe(false);
 });
 
@@ -51,50 +76,50 @@ test("isGenerator", () => {
     for (var i=0;; ++i)
       yield i;
   }
-  var a = funcxion.isGenerator(naturalNumbers);
+  var a = isGenerator(naturalNumbers);
   expect(a).toBe(true);
-  var a = funcxion.isGenerator(() => 0);
+  var a = isGenerator(() => 0);
   expect(a).toBe(false);
 });
 
 
 test("signature", () => {
-  // var a = funcxion.signature(funcxion.delay);
+  // var a = signature(delay);
   // expect(a).toBe("function delay(x, t)");
-  // var a = funcxion.signature(funcxion.debounce);
+  // var a = signature(debounce);
   // expect(a).toBe("function debounce()");
 });
 
 
 test("name", () => {
-  // var a = funcxion.name(funcxion.delay);
+  // var a = name(delay);
   // expect(a).toBe("delay");
-  // var a = funcxion.name(funcxion.debounce);
+  // var a = name(debounce);
   // expect(a).toBe("debounce");
 });
 
 
 test("parameters", () => {
-  // var a = funcxion.parameters(funcxion.delay);
+  // var a = parameters(delay);
   // expect(a).toStrictEqual(["x", "t"]);
-  // var a = funcxion.parameters(funcxion.debounce);
+  // var a = parameters(debounce);
   // expect(a).toStrictEqual(["x", "t", "T"]);
 });
 
 
 test("arity", () => {
-  // var a = funcxion.arity(funcxion.negate);
+  // var a = arity(negate);
   // expect(a).toBe(1);
-  // var a = funcxion.arity(funcxion.curry);
+  // var a = arity(curry);
   // expect(a).toBe(2);
-  // var a = funcxion.arity(funcxion.limitUse);
+  // var a = arity(limitUse);
   // expect(a).toBe(3);
 });
 
 
 test("bind.1", () => {
   var a  = [1];
-  var fn = funcxion.bind(Array.prototype.push, a);
+  var fn = bind(Array.prototype.push, a);
   fn(2, 3, 4);  // push(2, 3, 4)
   expect(a).toStrictEqual([1, 2, 3, 4]);
 });
@@ -102,17 +127,17 @@ test("bind.1", () => {
 
 test("bind.2", () => {
   var a  = [1, 2, 3, 4];
-  var fn = funcxion.bind(Array.prototype.splice, a, 1);
+  var fn = bind(Array.prototype.splice, a, 1);
   fn(2);  // splice(1, 2)
   expect(a).toStrictEqual([1, 4]);
 });
 
 
 test("negate", () => {
-  var fn = funcxion.negate(isFinite);
+  var fn = negate(isFinite);
   expect(fn(Infinity)).toBe(true);
   expect(fn(1)).toBe(false);
-  var fn = funcxion.negate(isNaN);
+  var fn = negate(isNaN);
   expect(fn(1)).toBe(true);
   expect(fn(NaN)).toBe(false);
 });
@@ -128,7 +153,7 @@ test("memoize.1", () => {
     ++calls;
     return factorialRec(n);
   }
-  var fn = funcxion.memoize(factorial);
+  var fn = memoize(factorial);
   expect(fn(3)).toBe(6);
   expect(fn(4)).toBe(24);
   expect(fn(5)).toBe(120);
@@ -148,7 +173,7 @@ test("memoize.2", () => {
   function resolver(x: number, y: number) {
     return 4093*y + x;  // a hash
   }
-  var fn = funcxion.memoize(hypot, resolver);
+  var fn = memoize(hypot, resolver);
   expect(fn(3,  4)).toBe(5);
   expect(fn(6,  8)).toBe(10);
   expect(fn(5, 12)).toBe(13);
@@ -164,7 +189,7 @@ test("reverse", () => {
   function divide(x: number, y: number) {
     return x/y;
   }
-  var fd = funcxion.reverse(divide);
+  var fd = reverse(divide);
   expect(fd(2, 4)).toBe(2);
   expect(fd(2, 6)).toBe(3);
   expect(fd(2, 8)).toBe(4);
@@ -172,7 +197,7 @@ test("reverse", () => {
   function push(...args: number[]) {
     array.push(...args);
   }
-  var fp = funcxion.reverse(push);
+  var fp = reverse(push);
   fp(2, 3, 4);  // push(2, 3, 4) in reverse order
   expect(array).toStrictEqual([1, 4, 3, 2]);
 });
@@ -185,75 +210,75 @@ test("spread", () => {
       a += v;
     return a;
   }
-  var fn = funcxion.spread(sum);
+  var fn = spread(sum);
   expect(fn(1, 2, 3)).toBe(6);                      // sum([1, 2, 3])
   var array = [1];
   function concat(x: number[]) {
     return array.concat(x);
   }
-  var fn = funcxion.spread(concat);
+  var fn = spread(concat);
   expect(fn(2, 3, 4)).toStrictEqual([1, 2, 3, 4]);  // concat([2, 3, 4])
 });
 
 
 test("unspread", () => {
-  var fn = funcxion.unspread(Math.min);
+  var fn = unspread(Math.min);
   expect(fn([7, 4, 9])).toBe(4);                        // Math.min(7, 4, 9)
-  var fn = funcxion.unspread((x, i, I) => x.slice(i, I));
+  var fn = unspread((x, i, I) => x.slice(i, I));
   expect(fn([[1, 2, 3, 4], 2])).toStrictEqual([3, 4]);  // [1, 2, 3, 4].slice(2)
 });
 
 
 test("wrap", () => {
-  var fn = funcxion.wrap(Math.min, 1);
+  var fn = wrap(Math.min, 1);
   expect(fn(100, 180, 130)).toBe(130);      // Math.min(180, 130)
-  var fn = funcxion.wrap(Math.min, 1, 3);
+  var fn = wrap(Math.min, 1, 3);
   expect(fn(100, 180, 130, 80)).toBe(130);  // Math.min(180, 130)
 });
 
 
 test("unwrap", () => {
-  var fn = funcxion.unwrap(Math.min, [100]);
+  var fn = unwrap(Math.min, [100]);
   expect(fn(180, 130)).toBe(100);  // Math.min(100, 180, 130)
   var array = [1];
-  var fn = funcxion.unwrap((...vs) => array.push(...vs), [10], [10]);
+  var fn = unwrap((...vs) => array.push(...vs), [10], [10]);
   fn(1, 2, 3);                     // array.push(10, 1, 2, 3, 10)
   expect(array).toStrictEqual([1, 10, 1, 2, 3, 10]);
 });
 
 
 test("compose", () => {
-  var fn = funcxion.compose(Math.sqrt, Math.abs);
+  var fn = compose(Math.sqrt, Math.abs);
   expect(fn(-64)).toBe(8);    // Math.sqrt(Math.abs(-64))
-  var fn = funcxion.compose(Math.sqrt, Math.min);
+  var fn = compose(Math.sqrt, Math.min);
   expect(fn(22, 9)).toBe(3);  // Math.sqrt(Math.min(22, 9))
 });
 
 
 test("composeRight", () => {
-  var fn = funcxion.composeRight(Math.abs, Math.sqrt);
+  var fn = composeRight(Math.abs, Math.sqrt);
   expect(fn(-64)).toBe(8);    // Math.sqrt(Math.abs(-64))
-  var fn = funcxion.composeRight(Math.min, Math.sqrt);
+  var fn = composeRight(Math.min, Math.sqrt);
   expect(fn(22, 9)).toBe(3);  // Math.sqrt(Math.min(22, 9))
 });
 
 
 test("curry", () => {
   var sub = (x: number, y: number) => x - y;
-  var fn  = funcxion.curry(sub);
+  var fn  = curry(sub);
   expect(fn(2)(3)).toBe(-1);    // sub(2, 3)
-  var fn  = funcxion.curry(Math.min, 3);
+  var fn  = curry(Math.min, 3);
   expect(fn(5)(8)(3)).toBe(3);  // Math.min(5, 8, 3)
 });
 
 
 test("curryRight", () => {
   var sub = (x: number, y: number) => x - y;
-  var fn  = funcxion.curryRight(sub);
+  var fn  = curryRight(sub);
   expect(fn(2)(3)).toBe(1);  // sub(3, 2)
   var array = [1];
   var push2 = (x: number, y: number) => array.push(x, y);
-  var fn  = funcxion.curryRight(push2, 2);
+  var fn  = curryRight(push2, 2);
   fn(2)(3);                  // push2(3, 2)
   expect(array).toStrictEqual([1, 3, 2]);
 });
@@ -261,7 +286,7 @@ test("curryRight", () => {
 
 test("delay.1", async () => {
   var count = 0;
-  var fn = funcxion.delay(() => ++count, 50);
+  var fn = delay(() => ++count, 50);
   setTimeout(fn, 0);    // `count` incremented after 50 ms
   setTimeout(fn, 100);  // `count` incremented after 150 ms
   setTimeout(() => expect(count).toBe(0), 20);
@@ -275,7 +300,7 @@ test("delay.1", async () => {
 
 test("delay.2", async () => {
   var count = 0;
-  var fn = funcxion.delay(() => ++count, 50);
+  var fn = delay(() => ++count, 50);
   setTimeout(fn, 0);    // `count` incremented after 50 ms
   setTimeout(fn, 100);  // `count` incremented after 150 ms
   setTimeout(fn, 200);  // `count` incremented after 250 ms
@@ -296,7 +321,7 @@ test("delay.2", async () => {
 test("limitUse.1", () => {
   var sum = 0;
   var add = (x: number) => sum += x;
-  var fn  = funcxion.limitUse(add, 0, 4);
+  var fn  = limitUse(add, 0, 4);
   for (var i=0; i<10; ++i)
     fn(i);
   expect(sum).toBe(6);  // 0 + 1 + 2 + 3
@@ -306,7 +331,7 @@ test("limitUse.1", () => {
 test("limitUse.2", () => {
   var sum = 0;
   var add = (x: number) => sum += x;
-  var fn  = funcxion.limitUse(add, 4, 8);
+  var fn  = limitUse(add, 4, 8);
   for (var i=0; i<10; ++i)
     fn(i);
   expect(sum).toBe(22);  // 4 + 5 + 6 + 7
@@ -315,7 +340,7 @@ test("limitUse.2", () => {
 
 test("debounce.1", async () => {
   var count = 0;
-  var fn = funcxion.debounce(() => ++count, 150);
+  var fn = debounce(() => ++count, 150);
   setTimeout(fn, 0);
   setTimeout(fn, 100);
   setTimeout(fn, 200);
@@ -344,7 +369,7 @@ test("debounce.1", async () => {
 
 test("debounce.2", async () => {
   var count = 0;
-  var fn = funcxion.debounce(() => ++count, 150);
+  var fn = debounce(() => ++count, 150);
   setTimeout(fn, 0);
   setTimeout(fn, 50);
   setTimeout(fn, 100);
@@ -376,7 +401,7 @@ test("debounce.2", async () => {
 
 test("debounceEarly.1", async () => {
   var count = 0;
-  var fn = funcxion.debounceEarly(() => ++count, 150);
+  var fn = debounceEarly(() => ++count, 150);
   setTimeout(fn, 0);
   setTimeout(fn, 100);
   setTimeout(fn, 200);
@@ -400,7 +425,7 @@ test("debounceEarly.1", async () => {
 
 test("debounceEarly.2", async () => {
   var count = 0;
-  var fn = funcxion.debounceEarly(() => ++count, 150);
+  var fn = debounceEarly(() => ++count, 150);
   setTimeout(fn, 0);
   setTimeout(fn, 50);
   setTimeout(fn, 100);
@@ -427,7 +452,7 @@ test("debounceEarly.2", async () => {
 
 test("throttle.1", async () => {
   var count = 0;
-  var fn = funcxion.throttle(() => ++count, 250);
+  var fn = throttle(() => ++count, 250);
   setTimeout(fn, 0);
   setTimeout(fn, 100);
   setTimeout(fn, 200);
@@ -445,7 +470,7 @@ test("throttle.1", async () => {
 
 test("throttle.2", async () => {
   var count = 0;
-  var fn = funcxion.throttle(() => ++count, 250);
+  var fn = throttle(() => ++count, 250);
   setTimeout(fn, 0);
   setTimeout(fn, 100);
   setTimeout(fn, 200);
@@ -472,7 +497,7 @@ test("throttle.2", async () => {
 
 test("throttleEarly.1", async () => {
   var count = 0;
-  var fn = funcxion.throttleEarly(() => ++count, 250);
+  var fn = throttleEarly(() => ++count, 250);
   setTimeout(fn, 0);
   setTimeout(fn, 100);
   setTimeout(fn, 200);
@@ -485,7 +510,7 @@ test("throttleEarly.1", async () => {
 
 test("throttleEarly.2", async () => {
   var count = 0;
-  var fn = funcxion.throttleEarly(() => ++count, 250);
+  var fn = throttleEarly(() => ++count, 250);
   setTimeout(fn, 0);
   setTimeout(fn, 100);
   setTimeout(fn, 200);
